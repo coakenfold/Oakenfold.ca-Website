6a09e7ca362d80bdca951eec8fcf11e625a32ff9
Chad Oakenfold <web@oakenfold.ca>
2018-06-03 22:35:39 -0700

    Razing to blank slate

--- grunt/tasks/default.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-    // @@ grunt
-    //
-    // Default task. Processes files in ./development & outputs to ./deploy
-    //
-    grunt.registerTask('default', [
-        'watch-all-task'
-    ]);
-};
--- grunt/tasks/watch-css.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-    // @@ grunt development-watch-css
-    //
-    // NOTE: launches a livereload server for auto refreshes in the browser
-    //
-    grunt.registerTask('watch-css', [
-        'watch:css'
-    ]);
-};
